c05be7574d7e7d2e2efcf0ed2422d014cc67f2e6
tyurikovmyu <TyurikovMYU@polyus.com>
2026-03-17 12:22:05 +0300

    1st ddm

---
+++ askb_ch/macros/generate_schema_name.sql
@@ -0,0 +1,14 @@
+{#- переопределен стандартный макрос чтобы не использовались схемы с лишним STG_ -#}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
